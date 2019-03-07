268b8ed0629bb728ef1e65b29591167e0cfad4e4
anton <anton@dubyna.me>
2019-03-06 16:00:30 -0800

    Iris layout v1

---
+++ keyboards/iris/keymaps/bitshape/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define USE_I2C
+
+#define MASTER_LEFT
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM    200
